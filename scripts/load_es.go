b7584accf383fc48fc809c7a0088827955ce3014
Jorden Kawamoto <jkawamoto@healthgrades.com>
2019-03-21 14:58:37 -0600

    commit before refactor. turnback point

--- scripts/load_es.go
+++ scripts/load_es.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
+	"context"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
-	"strings"
-	"time"
+
+	"github.com/elastic/go-elasticsearch/esapi"
+
+	"github.com/elastic/go-elasticsearch"
 )
 
 const esHost = "localhost:9200"
@@ -16,55 +19,103 @@ const timeoutSeconds = 2 * 60
 
 func main() {
 	// Wait for cluster to be ready
-	startTime := time.Now().Unix()
-	timeoutReached := false
+	// startTime := time.Now().Unix()
+	// timeoutReached := false
 
-	for !timeoutReached {
-		res, err := http.Get(fmt.Sprintf("http://%s/_cat/health", esHost))
-		if err != nil {
-			fmt.Printf("The HTTP request failed with error %s\n", err)
-		} else {
-			data, _ := ioutil.ReadAll(res.Body)
-			stringSlice := strings.Split(string(data), " ")
-			// contains() is a little safer than a regex check
-			if contains(stringSlice, "green") || contains(stringSlice, "yellow") {
-				fmt.Println("ES ready for indexing!")
-				break
-			}
-		}
-		fmt.Println("Waiting for Elasticsearch to be ready for requests...")
-		time.Sleep(5 * time.Second)
-		timeoutReached = (time.Now().Unix() - startTime) > timeoutSeconds
-	}
+	// for !timeoutReached {
+	// 	res, err := http.Get(fmt.Sprintf("http://%s/_cat/health", esHost))
+	// 	if err != nil {
+	// 		fmt.Printf("The HTTP request failed with error %s\n", err)
+	// 	} else {
+	// 		data, _ := ioutil.ReadAll(res.Body)
+	// 		stringSlice := strings.Split(string(data), " ")
+	// 		// contains() is a little safer than a regex check
+	// 		if contains(stringSlice, "green") || contains(stringSlice, "yellow") {
+	// 			fmt.Println("ES ready for indexing!")
+	// 			break
+	// 		}
+	// 	}
+	// 	fmt.Println("Waiting for Elasticsearch to be ready for requests...")
+	// 	time.Sleep(5 * time.Second)
+	// 	timeoutReached = (time.Now().Unix() - startTime) > timeoutSeconds
+	// }
 
-	if timeoutReached {
-		log.Fatalf("Timeout reached waiting for ES to be ready for requests")
-	}
+	// if timeoutReached {
+	// 	log.Fatalf("Timeout reached waiting for ES to be ready for requests")
+	// }
 
 	// Apply mappings
 	// Use the http client to create the index because the es client was throwing errors.
 	workingDir, _ := os.Getwd()
-	jsonFile, err := os.Open(workingDir + "/data/games-mappings.json")
+	// gamesMappingsFile, err := os.Open(workingDir + "/data/games-mappings.json")
+	// if err != nil {
+	// 	log.Fatalf("Error reading mappings: %s", err)
+	// }
+
+	// defer gamesMappingsFile.Close()
+
+	// client := &http.Client{}
+	// esPutURL := fmt.Sprintf("http://%s/%s", esHost, indexName)
+	// req, err := http.NewRequest(http.MethodPut, esPutURL, gamesMappingsFile)
+	// req.Header.Set("content-type", "application/json; charset=UTF-8")
+	// if err != nil {
+	// 	log.Fatalf("Error building request: %s", err)
+	// }
+	// resp, err := client.Do(req)
+	// if err != nil {
+	// 	log.Fatalf("Error indexing ES with PUT request: %s", err)
+	// }
+
+	// respBodyStr, _ := ioutil.ReadAll(resp.Body)
+	// fmt.Printf("Successfully created ES index. ES response:\n%s", respBodyStr)
+
+	// Bulk index data into Elasticsearch
+	gameDataFile, err := os.Open(workingDir + "/data/mlb-games.jsonl")
 	if err != nil {
-		log.Fatalf("Error reading mappings: %s", err)
+		log.Fatalf("Error reading data file: %s", err)
 	}
 
-	defer jsonFile.Close()
+	defer gameDataFile.Close()
 
-	client := &http.Client{}
-	esPutURL := fmt.Sprintf("http://%s/%s", esHost, indexName)
-	req, err := http.NewRequest(http.MethodPut, esPutURL, jsonFile)
-	req.Header.Set("content-type", "application/json; charset=UTF-8")
+	esClient, err := elasticsearch.NewDefaultClient()
 	if err != nil {
-		log.Fatalf("Error building request: %s", err)
+		log.Fatalf("Error building ES client: %s", err)
 	}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatalf("Error putting request: %s", err)
+
+	fileReader := bufio.NewReader(gameDataFile)
+	esDataReader := bytes.NewReader([]byte{})
+	count := 0
+	for {
+		bytesLine, prefix, _ := fileReader.ReadLine()
+		count++
+		// break at file end
+		if len(bytesLine) == 0 && !prefix {
+			resp, err := esapi.BulkRequest{
+				Index: indexName,
+				Body:  esDataReader,
+			}.Do(context.Background(), esClient)
+			fmt.Println(resp)
+
+			fmt.Println(err)
+			break
+		}
+		// err check?
+		i, _ := esDataReader.Read(bytesLine)
+		fmt.Println(i)
+		if count%500 == 0 {
+			resp, err := esapi.BulkRequest{
+				Index: indexName,
+				Body:  esDataReader,
+			}.Do(context.Background(), esClient)
+
+			esDataReader = bytes.NewReader([]byte{})
+
+			fmt.Println(resp)
+
+			fmt.Println(err)
+		}
 	}
-	str, _ := ioutil.ReadAll(resp.Body)
-	log.Println(string(str))
-	// Add data
+
 }
 
 func contains(slice []string, value string) bool {
